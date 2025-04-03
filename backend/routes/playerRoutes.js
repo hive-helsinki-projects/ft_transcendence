28277066a736957a517969ffe942a31553c00850
k2matu <kstt90@gmail.com>
2025-04-03 13:08:57 +0300

    added getting one player and also creating player

--- backend/routes/playerRoutes.js
+++ backend/routes/playerRoutes.js
@@ -1,5 +1,5 @@
 import playerController from '../controllers/playerController.js'
-import { getPlayersOpts } from '../models/playerSchemas.js';
+import { getPlayersOpts, getPlayerOpts, postPlayerOpts } from '../models/playerSchemas.js';
 
 function playerRoutes(fastify, options) {
 	// Get all players belonging to a user
@@ -8,6 +8,20 @@ function playerRoutes(fastify, options) {
 		onRequest: [fastify.jwtAuth],
 		handler: playerController.getPlayers 
 	})
+	
+	// Get a player belonging to a user
+	fastify.get('/players/:user_id/:player_id', {
+		...getPlayerOpts,
+		onRequest: [fastify.jwtAuth],
+		handler: playerController.getPlayer
+	})
+	
+	// Create a player
+	fastify.post('/players/:id', {
+		...postPlayerOpts,
+		onRequest: [fastify.jwtAuth],
+		handler: playerController.createPlayer
+	}) 
 }
 
 export default playerRoutes;
