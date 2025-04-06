c0c8ed19ae59a16d0e854dd08617b761a2490217
lumikil <kilpelainenlumi@gmail.com>
2025-04-06 11:55:06 +0300

    Deleted duplicated files and folder, resovle conflict  server.js

--- backend/controllers/game.controller.js
+++
@@ -1,9 +0,0 @@
-const handleWebSocket = (connection, request) => {
-    connection.socket.on('message', (message) => {
-      console.log('Received:', message);
-      connection.socket.send(`Echo: ${message}`);
-    });
-  };
-  
-  export default { handleWebSocket };
-  
--- backend/routes/user.routes.js
+++
@@ -1,8 +0,0 @@
-import userController from '../controllers/user.controller.js';
-
-async function userRoutes(fastify, options) {
-  fastify.get('/users', userController.getUsers);
-  fastify.post('/users', userController.createUser);
-}
-
-export default userRoutes;
