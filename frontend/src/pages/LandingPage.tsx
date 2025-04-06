c9cfe70328ab483c1c87e1f8c0749f1efc9420d2
lumikil <kilpelainenlumi@gmail.com>
2025-04-06 13:48:33 +0300

    Add JWT authentication flow and protect routes in frontend

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -8,12 +8,12 @@ const LandingPage: React.FC = () => {
         <h1>Ping. Pong. Play!</h1>
         <h2>Level Up Your Ping Pong Skills</h2>
         <p>
-          Smash, spin, and dominate the table. Prove you’re the ultimate paddle master.
+          Smash, spin, and dominate the table. Prove you're the ultimate paddle master.
         </p>
 
         <div>
-          <Card title="For existing users" buttonText="Let’s Play" navigateTo="/dashboard"/>
-          <Card title="Don’t have an account?" buttonText="Register" navigateTo="/register" />
+          <Card title="For existing users" buttonText="Let's Play" navigateTo="/dashboard"/>
+          <Card title="Don't have an account?" buttonText="Register" navigateTo="/register" />
         </div>
       </div>
     </div>
