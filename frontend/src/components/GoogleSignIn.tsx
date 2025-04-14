a2313aed6a7b6522050558b7e809ddf7eac49d4c
ito-miyuki <mitou.1221@gmail.com>
2025-04-14 18:28:08 +0300

    get the client ID and inpemented

--- frontend/src/components/GoogleSignIn.tsx
+++ frontend/src/components/GoogleSignIn.tsx
@@ -17,7 +17,8 @@ const GoogleSignIn: React.FC<GoogleSignInProps> = ({ isLoading }) => {
         if (window.gapi) {
             window.gapi.load('auth2', () => {
                 window.gapi.auth2.init({
-                    client_id: 'YOUR_GOOGLE_CLIENT_ID' // modify this part
+                    client_id: '847383291975-9ten21d8j1vf3m2m1kod2i2js9c28o6e.apps.googleusercontent.com', // modify this part
+                    redirect_uri: 'http://localhost:5173/dashboard'
                 });
             });
         } else {
@@ -33,6 +34,8 @@ const GoogleSignIn: React.FC<GoogleSignInProps> = ({ isLoading }) => {
                 const id_token = googleUser.getAuthResponse().id_token;
                 console.log('Google TD Token: ', id_token);
                 // send the token to the server
+                // sendTokenToBackend(id_token);
+                // navigate('/dashboard') // do we need it?
             },
             (error: any) => {
                 console.log('Login error: ', error); // do error handlings
