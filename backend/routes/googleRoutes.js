9d750701e5fe612ebb05858ba0dcb0baa0030c3c
ito-miyuki <mitou.1221@gmail.com>
2025-04-28 13:35:36 +0300

    removed monnents and console log

--- backend/routes/googleRoutes.js
+++ backend/routes/googleRoutes.js
@@ -1,27 +1,19 @@
-// リクエストを受け取り、どのコントローラーハンドラーを呼び出すかを決定する。
-// ルートの定義と、そのルートに対応するコントローラー関数を関連付ける。
-
 import db from '../models/database.js';
 import bcrypt from 'bcryptjs';
 
 import verifyGoogleToken from '../controllers/googleController.js';
 
 async function googleRoutes(fastify, options) {
-    console.log("Hi from googleRoutes fucntion"); // for debugging
 
     fastify.post('/api/auth/google', async (request, reply) => {
         let { token } = request.body;
-        console.log('we recieved this token', token);
 
         try {
             let user = await verifyGoogleToken(token);
-            console.log("HERE IS THE USER INFO:", user);
 
-            // CREATE A USER WITH INFORMATION FROM THE GOOGLE (EMAIL, USERNAME); LINE 69 IN AUTHCONTROLLER
             const userExist = db.prepare(`SELECT * FROM users WHERE email = ?`).get(user.email);
             if (!userExist) {
-                const result = db.prepare(`INSERT INTO users (username, email) VALUES (?, ?)`).run(user.given_name, user.email);
-                console.log('result:', result);
+                db.prepare(`INSERT INTO users (username, email) VALUES (?, ?)`).run(user.given_name, user.email);
             }
 
             user = db.prepare(`SELECT * FROM users WHERE email = ? AND username = ?`).get(user.email, user.given_name);
@@ -33,10 +25,8 @@ async function googleRoutes(fastify, options) {
                 username: user.username,
                 id: user.id,
             };
-            // OR IF USER EXIST. SIGN IN AND SEND BACK THE TOKEN.
 
             token = await reply.jwtSign(userForToken, { expiresIn: '1h'});
-            // MAKE A TOKEN TO SEND BACK TO FRONTEND. AUTHCONTROLLER.JS LINE 18-34
 
             db.prepare(`UPDATE users
                 SET online_status = TRUE
@@ -44,8 +34,6 @@ async function googleRoutes(fastify, options) {
                 AND online_status = FALSE
                 `).run(user.id);
             
-                console.log("HERE IS TOKEN:", token)
-            // SEND BACK USERNAME AND TOKEN TO FRONTEND
             return reply.send({
                 message: 'Verified',
                 user: { token, username: user.username},
