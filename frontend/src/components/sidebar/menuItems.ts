01f6ed253443c37c182f3d7d212d6e8901e53030
k2matu <kstt90@gmail.com>
2025-05-23 12:57:15 +0300

    changed the sidebar user to user profile

--- frontend/src/components/sidebar/menuItems.ts
+++ frontend/src/components/sidebar/menuItems.ts
@@ -9,10 +9,12 @@ import {
 
 import { type MenuItem } from './SidebarItem';
 
+const id = localStorage.getItem('id');;
+
 // Configuration
 export const menuItems: MenuItem[] = [
     { id: 'home', icon: Home, path: '/' },
-    { id: 'user', icon: User, path: '/dashboard' },
+    { id: 'user', icon: User, path: `/profile/${id}`},
     { id: 'game', icon: Gamepad2, path: '/game' },
     { id: 'trophy', icon: Trophy, path: '/tournament' },
     { id: 'settings', icon: Settings, path: '/settings' },
