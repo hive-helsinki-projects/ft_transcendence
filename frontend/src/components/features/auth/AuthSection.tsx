420dd4489e1ee78781734b812725d8200af0945a
ito-miyuki <mitou.1221@gmail.com>
2025-05-28 15:28:25 +0300

    changed to redirect!

--- frontend/src/components/features/auth/AuthSection.tsx
+++ frontend/src/components/features/auth/AuthSection.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { AuthSectionProps } from '../../../types/auth'
 import '../../../assets/styles/index.css'
-import GoogleSignIn from "../../../components/GoogleSignIn";
+import GoogleSignInNew from "../../../components/GoogleSignInNew";
 import { useAuthForm } from '../../../hooks/auth/useAuthForm'
 
 const AuthSection: React.FC<AuthSectionProps> = ({
@@ -20,9 +20,7 @@ const AuthSection: React.FC<AuthSectionProps> = ({
     <div className="auth-options">
       <div className="google-auth">
         <span>Or</span>
-          <GoogleSignIn 
-              // isLoading={isLoading}
-            />
+          <GoogleSignInNew />
       </div>
 
       <div className="register-link">
