b26e8c36103929c9187573b350899b5fb086f931
lumikil <kilpelainenlumi@gmail.com>
2025-06-07 14:28:37 +0300

    Remove App component and update import path in main.tsx to reflect new structure

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './app'
+import App from '@/app/App'
 import './assets/styles/index.css'
 import './i18n/config'
 
