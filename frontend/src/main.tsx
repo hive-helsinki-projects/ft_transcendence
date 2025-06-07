1b47180fc72395b0f8031ae771f669bcc482f7f0
lumikil <kilpelainenlumi@gmail.com>
2025-06-07 14:30:42 +0300

    Update TypeScript configuration and refactor main.tsx for improved module handling
    - Added "allowSyntheticDefaultImports" to tsconfig.app.json to support default imports from modules without a default export.
    - Refactored main.tsx to use createRoot from 'react-dom/client' for rendering the App component, enhancing compatibility with React 18.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
 import App from '@/app/App'
+import React from 'react'
+import { createRoot } from 'react-dom/client'
 import './assets/styles/index.css'
 import './i18n/config'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
