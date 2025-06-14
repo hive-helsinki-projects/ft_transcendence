0c703a3e6d02d11e0e8a01e2be970bc7a24ac687
Vallehtelia <valle.vaalanti@gmail.com>
2025-06-14 17:41:08 +0300

    added finnish translation

--- frontend/src/i18n/config.ts
+++ frontend/src/i18n/config.ts
@@ -3,13 +3,15 @@ import { initReactI18next } from "react-i18next";
 
 import en from './locales/en.json';
 import ja from './locales/ja.json';
+import fi from './locales/fi.json';
 
 i18n
     .use(initReactI18next)
     .init({
         resources: {
             en: { translation: en },
-            ja: { translation: ja }
+            ja: { translation: ja },
+            fi: { translation: fi }
         },
         lng: 'en',
         fallbackLng: 'en',
