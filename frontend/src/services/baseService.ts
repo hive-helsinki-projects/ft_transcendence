dd8390fb60f291d4842ea1f8a46d35c48e76c3d0
Lumi K <149065128+lkilpela@users.noreply.github.com>
2025-06-22 00:05:13 +0300

    Update baseService.ts

--- frontend/src/services/baseService.ts
+++ frontend/src/services/baseService.ts
@@ -1,22 +1,22 @@
 import api from './axiosAgent';
 
 export class BaseService {
-  protected static async get<T>(endpoint: string, params?: object): Promise<T> {
+  public static async get<T>(endpoint: string, params?: object): Promise<T> {
     const response = await api.get<T>(endpoint, { params });
     return response.data;
   }
 
-  protected static async post<T>(endpoint: string, data?: object): Promise<T> {
+  public static async post<T>(endpoint: string, data?: object): Promise<T> {
     const response = await api.post<T>(endpoint, data);
     return response.data;
   }
 
-  protected static async put<T>(endpoint: string, data?: object): Promise<T> {
+  public static async put<T>(endpoint: string, data?: object): Promise<T> {
     const response = await api.put<T>(endpoint, data);
     return response.data;
   }
 
-  protected static async delete<T>(endpoint: string): Promise<T> {
+  public static async delete<T>(endpoint: string): Promise<T> {
     const response = await api.delete<T>(endpoint);
     return response.data;
   }
