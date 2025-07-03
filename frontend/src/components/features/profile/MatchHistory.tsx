25a39270dfe423be0747faabc974ac214acc1429
Oliver Hertzberg <oliverhertzberg@hotmail.com>
2025-07-03 17:22:41 +0300

    add ts for MatchHistory.tsx

--- frontend/src/components/features/profile/MatchHistory.tsx
+++ frontend/src/components/features/profile/MatchHistory.tsx
@@ -2,13 +2,31 @@ import React from 'react';
 import axios from 'axios';
 import PlayerName from './PlayerName';
 
+
+interface MatchPlayer {
+    player_id: number;
+    score: number;
+}
+  
+interface Match {
+    id: number;
+    type: string;
+    tournament_id: number;
+    date: string;
+    round: number;
+    status: string;
+    winner_id: number;
+    players: MatchPlayer[];
+}
+
+
 const MatchHistory = ({ userId }: { userId: string }) => {
-    const [matches, setMatches] = React.useState<any[]>([]);
+    const [matches, setMatches] = React.useState<Match[]>([]);
 
     React.useEffect(() => {
         const fetchMatchHistory = async () => {
             try {
-                const response = await axios.get(`https://localhost:3001/match-histories/user/${userId}`);
+                const response = await axios.get<Match[]>(`https://localhost:3001/match-histories/user/${userId}`);
                 setMatches(response.data);
                 console.log('Match history fetched:', response.data);
             } catch (error) {
@@ -26,10 +44,10 @@ const MatchHistory = ({ userId }: { userId: string }) => {
                     {matches.map((match) => (
                         <li key={match.id}>
                             <p>Match Date: {match.date}</p>
-                            <p>Winner: <PlayerName id={match.winner_id} />  </p>
-                            <p>Player: <PlayerName id={match.players[0].player_id} /> Score: {match.players[0].score}</p>
+                            <p>Winner: <PlayerName id={String(match.winner_id)} />  </p>
+                            <p>Player: <PlayerName id={String(match.players[0].player_id)} /> Score: {match.players[0].score}</p>
                             <p>VS</p>
-                            <p>Player: <PlayerName id={match.players[1].player_id} /> Score: {match.players[1].score} </p>
+                            <p>Player: <PlayerName id={String(match.players[1].player_id)} /> Score: {match.players[1].score} </p>
                         </li>
                     ))}
                 </ul>
