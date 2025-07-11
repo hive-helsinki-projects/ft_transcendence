9c90f99b08120ae497c049e42e9d3bbc565d3494
lkilpela <lkilpela@student.hive.fi>
2025-07-11 19:10:11 +0300

    Remove unused help sections: FAQSection, RulesSection, ContactSection, and FeedbackSection from the project.

--- frontend/src/pages/Help.tsx
+++ frontend/src/pages/Help.tsx
@@ -1,9 +1,7 @@
 import { FAQ, gameRules, troubleshootingSteps } from '@utils/constants'
 import React from 'react'
 import {
-  ContactSection,
   FAQSection,
-  FeedbackSection,
   HelpSection,
   RulesSection,
   TroubleshootingSection,
@@ -22,20 +20,7 @@ export const Help: React.FC = () => {
       <RulesSection rules={gameRules} />
       <FAQSection faqs={FAQ} />
 
-      {/* <ContactSection
-        email="support@example.com"
-        title="Contact Support"
-        description="If you need further assistance, please contact us at"
-      /> */}
-
       <TroubleshootingSection steps={troubleshootingSteps} />
-
-      {/* <FeedbackSection
-        title="Feedback"
-        description="We value your feedback! Please let us know how we can improve by submitting your feedback"
-        linkText="here"
-        linkPath="/feedback"
-      /> */}
     </div>
   )
 }
